72689a25e5eee8c0388f072c2a874fd75673d2f0
MehdiMascret <mehdi@mascret.dev>
2026-01-05 16:26:53 +0100

    + funct: left and right

--- lua/keymap.lua
+++ lua/keymap.lua
@@ -2,7 +2,7 @@ vim.g.mapleader = " "
 
 -- Neotree
 vim.keymap.set('n', '<C-n>', ':Neotree<CR>', {})
-vim.keymap.set('n', '<leader>n', ':Neotree<CR>', {}) 
+vim.keymap.set('n', '<leader>n', ':Neotree<CR>', {})
 
 -- Windows
 for _, dir in ipairs({ "Up", "Down", "Left", "Right" }) do
@@ -40,3 +40,40 @@ vim.keymap.set("i", "<S-Tab>", "<C-d>", { silent = true })
 
 -- Indenter + insert
 vim.keymap.set("n", "<Tab>", ">>I", { silent = true })
+
+-- Aller à une autre ligne si position (END + Right) ou (HOME + Left)
+local function make_change_line(condition, action, other_action)
+  return function()
+    if condition() then
+      return other_action
+    end
+    return action
+  end
+end
+
+for _, m in ipairs({
+  { 
+    modes = "i", 
+    condition = function () return vim.api.nvim_win_get_cursor(0)[2] >= #vim.api.nvim_get_current_line() end, 
+    action = "<Right>", other_action = "<Home><Down>"
+  },
+  { 
+    modes = { "i", "n" }, 
+    condition = function () return vim.api.nvim_win_get_cursor(0)[2] <= 0 end, 
+    action = "<Left>", other_action = "<End><Up>"
+  },
+}) do
+  vim.keymap.set(
+    m.modes, 
+    m.action, 
+    make_change_line(m.condition, m.action, m.other_action), 
+    { expr = true, silent = true, replace_keycodes = true }
+  )
+end
+vim.keymap.set("n", "<Right>", 
+  make_change_line(
+    function ()
+      return vim.api.nvim_win_get_cursor(0)[2] >= #vim.api.nvim_get_current_line() - 1
+    end, "<Right>", "<Home><Down>"
+  ), { expr = true, silent = true, replace_keycodes = true }
+)
